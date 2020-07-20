6250391d5ed5edf42df0acd5d7a5d98841c2d9f3
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2020-07-20 17:23:35 -0300

    Create solve (#4)
    * Fix: Tests
    
    * Add: Dependencies and Metadatas
    
    * Add: Solve
    
    * Fix: Readme

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,7 +1,8 @@
 import newton
 import inspect
 import numpy as np
-from typing import Union, Tuple, Callable
+from numpy import ndarray
+from typing import Tuple, Callable, Union
 
 
 def test_newton_has_solve():
@@ -12,59 +13,35 @@ def test_newton_solve_is_callabe():
     inspect.isfunction(newton.solve)
 
 
-def test_newton_has_Vec():
-    assert hasattr(newton, "Vec")
-
-
-def test_newton_Vec_Type():
-    assert newton.Vec is Union[int, float, np.ndarray]
-
-
-def test_newton_has_Func():
-    assert hasattr(newton, "Func")
-
-
-def test_newton_Func_Type():
-    assert newton.Func is Callable[[newton.Vec], newton.Vec]
-
-
-def test_newton_has_Result():
-    assert hasattr(newton, "Result")
-
-
-def test_newton_Result_Type():
-    assert newton.Result is Tuple[bool, newton.Vec, newton.Vec]
-
-
 class TestSolveSignature:
     sig = inspect.signature(newton.solve)
 
     def test_solve_return_tuple(cls):
-        assert cls.sig.return_annotation is newton.Result
+        assert cls.sig.return_annotation is Tuple[bool, ndarray, ndarray]
 
     def test_solve_has_param_func(cls):
         assert cls.sig.parameters["func"]
 
     def test_solve_param_func_is_callable(cls):
-        assert cls.sig.parameters["func"].annotation is newton.Func
+        assert cls.sig.parameters["func"].annotation is Callable[[ndarray], ndarray]
 
     def test_solve_has_param_jacobian(cls):
         assert cls.sig.parameters["jacobian"]
 
     def test_solve_param_jacobian_is_callable(cls):
-        assert cls.sig.parameters["jacobian"].annotation is newton.Func
+        assert cls.sig.parameters["jacobian"].annotation is Callable[[ndarray], ndarray]
 
     def test_solve_has_param_x0(cls):
         assert cls.sig.parameters["x0"]
 
-    def test_solve_param_x0_is_Vec(cls):
-        assert cls.sig.parameters["x0"].annotation is newton.Vec
+    def test_solve_param_x0_is_ndarray(cls):
+        assert cls.sig.parameters["x0"].annotation is np.ndarray
 
     def test_solve_has_param_tol(cls):
         assert cls.sig.parameters["tol"]
 
-    def test_solve_param_tol_is_union_Vec(cls):
-        assert cls.sig.parameters["tol"].annotation is newton.Vec
+    def test_solve_param_tol_is_union(cls):
+        assert cls.sig.parameters["tol"].annotation is Union[int, float, ndarray]
 
     def test_solve_has_param_maxiter(cls):
         assert cls.sig.parameters["maxiter"]
@@ -80,11 +57,11 @@ class TestSolveSignature:
 
 
 class TestSolveOneVariable:
-    def test_solve_converge():
+    def test_solve_converge(self):
         result = newton.solve(
-            lambda x: x ** 2,
-            lambda x: 2 * x,
-            x0=1.2,
+            lambda x: np.array([x ** 2]),
+            lambda x: np.array([2 * x]),
+            x0=np.array([1.2]),
             tol=0.001,
             maxiter=100,
             verbose=True,
@@ -94,11 +71,11 @@ class TestSolveOneVariable:
         assert result[1] <= 0.001 or result[1] >= -0.001
         assert result[2] <= 0.001 or result[2] >= -0.001
 
-    def test_solver_not_coverge():
+    def test_solver_not_coverge(self):
         result = newton.solve(
-            lambda x: x ** 2,
-            lambda x: 2 * x,
-            x0=1000,
+            lambda x: np.array([x ** 2]),
+            lambda x: np.array([2 * x]),
+            x0=np.array([100]),
             tol=1e-10,
             maxiter=2,
             verbose=True,
@@ -108,12 +85,12 @@ class TestSolveOneVariable:
 
 
 class TestSolveMultVariable:
-    def test_solve_converge():
+    def test_solve_converge(self):
         result = newton.solve(
-            lambda x: np.array([[x[0, 0] ** 2 + x[1, 0] ** 2]]),
-            lambda x: np.array([[x[0, 0] * 2, x[1, 0] * 2]]),
-            x0=np.array([[0.5], [0.5]]),
-            tol=0.001,
+            lambda x: np.array([x[0] ** 2 + x[1] ** 2, 2 * x[1]]),
+            lambda x: np.array([[2 * x[0], 2 * x[1]], [0, 2]]),
+            x0=np.array([1, 1]),
+            tol=np.array([0.001, 0.001]),
             maxiter=100,
             verbose=True,
         )
@@ -122,11 +99,11 @@ class TestSolveMultVariable:
         assert all(result[1] <= 0.001) or all(result[1] >= -0.001)
         assert all(result[2] <= 0.001) or all(result[2] >= -0.001)
 
-    def test_solve_not_converge():
+    def test_solve_not_converge(self):
         result = newton.solve(
-            lambda x: x[0, 0] ** 2 + x[1, 0] ** 2,
-            lambda x: np.array([[x[0, 0] * 2, 0], [0, x[1, 0] * 2]]),
-            x0=np.array([[500], [500]]),
+            lambda x: np.array([x[0] ** 2 + x[1] ** 2, 2 * x[1]]),
+            lambda x: np.array([[2 * x[0], 2 * x[1]], [0, 2]]),
+            x0=np.array([100, 100]),
             tol=1e-10,
             maxiter=2,
             verbose=True,
